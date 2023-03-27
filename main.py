d356a366de93a0b9be91cbbad7e359eca9a4fa22
Rianna Aalto <rianna.aalto@gmail.com>
2023-03-27 02:24:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def get_session():
 
 app = FastAPI()
 
+
 # welcome message at home page
 @app.get("/")
 def root():
@@ -35,11 +36,13 @@ def list_books(session: Session = Depends(get_session)):
 @app.get("/book-by-index/{bookId}")
 def get_book(bookId:int, session: Session = Depends(get_session)):
     item = session.query(models.Item).get(bookId)
+    if not item:
+        raise HTTPException(status_code=404, detail="Book not found")
     return item
 
     
 # create new book record
-@app.post("/")
+@app.post("/add-book")
 def add_book(item:schemas.Item, session: Session = Depends(get_session)):
     item = models.Item(title = item.title, author = item.author)
     session.add(item)
@@ -47,9 +50,11 @@ def add_book(item:schemas.Item, session: Session = Depends(get_session)):
     session.refresh(item)
     return item
 
-@app.put("/{bookId}")
+@app.put("/update-book/{bookId}")
 def update_book(bookId: int, item:schemas.Item, session: Session = Depends(get_session)):
     itemObject = session.query(models.Item).get(bookId)
+    if not itemObject:
+        raise HTTPException(status_code=404, detail="Book not found")
     itemObject.title = item.title
     itemObject.author = item.author
     session.commit()
@@ -57,13 +62,12 @@ def update_book(bookId: int, item:schemas.Item, session: Session = Depends(get_s
     return itemObject
 
 #delete a book
-@app.delete("/{BookId}")
+@app.delete("/delete-book/{BookId}")
 def delete_book(bookId:int, session: Session = Depends(get_session)):
     itemObject = session.query(models.Item).get(bookId)
+    if not itemObject:
+        raise HTTPException(status_code=404, detail="Book not found")
     session.delete(itemObject)
     session.commit()
     session.close()
     return  'The book is now deleted!'
-
-
-
